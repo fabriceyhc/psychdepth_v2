7ee79daec61c14182a5b52bfc8ceba84d430cfcc
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2025-02-24 16:39:22 -0800

    partial results for deepseek mipro prompt engineering

--- story_eval/dspy/run.sh
+++ story_eval/dspy/run.sh
@@ -4,8 +4,8 @@
 # CUDA_VISIBLE_DEVICES=0 python -m story_eval.dspy.singlescore.annotate --model_id deepseek-ai/DeepSeek-R1-Distill-Llama-8B
 # CUDA_VISIBLE_DEVICES=0 python -m story_eval.dspy.multiscore.annotate  --model_id deepseek-ai/DeepSeek-R1-Distill-Llama-8B
 
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4 python -m story_eval.dspy.singlescore.annotate --model_id meta-llama/Llama-3.1-70B-Instruct
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4 python -m story_eval.dspy.multiscore.annotate  --model_id meta-llama/Llama-3.1-70B-Instruct
+# CUDA_VISIBLE_DEVICES=1,2,3,4 python -m story_eval.dspy.singlescore.annotate --model_id meta-llama/Llama-3.1-70B-Instruct
+# CUDA_VISIBLE_DEVICES=1,2,3,4 python -m story_eval.dspy.multiscore.annotate  --model_id meta-llama/Llama-3.1-70B-Instruct
 
 CUDA_VISIBLE_DEVICES=1,2,3,4 python -m story_eval.dspy.singlescore.annotate --model_id deepseek-ai/DeepSeek-R1-Distill-Llama-70B
 CUDA_VISIBLE_DEVICES=1,2,3,4 python -m story_eval.dspy.multiscore.annotate  --model_id deepseek-ai/DeepSeek-R1-Distill-Llama-70B
